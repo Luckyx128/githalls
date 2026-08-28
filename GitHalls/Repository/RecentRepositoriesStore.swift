82982a53ab6bac7394760f106e0fbc9b9d0f952c
Lucas Amorim <lucas.amorim@ambiensys.com.br>
2026-08-28 16:02:28 -0300

    deat

--- GitHalls/Repository/RecentRepositoriesStore.swift
+++ GitHalls/Repository/RecentRepositoriesStore.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+enum RecentRepositoriesStore {
+    private static let key = "recentRepositoryPaths"
+    private static let maxCount = 10
+
+    static func load() -> [URL] {
+        let paths = UserDefaults.standard.stringArray(forKey: key) ?? []
+        return paths.map { URL(fileURLWithPath: $0) }
+    }
+
+    static func addOrPromote(_ url: URL) {
+        var paths = UserDefaults.standard.stringArray(forKey: key) ?? []
+        paths.removeAll { $0 == url.path }
+        paths.insert(url.path, at: 0)
+        if paths.count > maxCount {
+            paths = Array(paths.prefix(maxCount))
+        }
+        UserDefaults.standard.set(paths, forKey: key)
+    }
+
+    static func remove(_ url: URL) {
+        var paths = UserDefaults.standard.stringArray(forKey: key) ?? []
+        paths.removeAll { $0 == url.path }
+        UserDefaults.standard.set(paths, forKey: key)
+    }
+}
