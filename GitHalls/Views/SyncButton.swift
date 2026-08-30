e86dbf81bec6708e0788d1806bfccfec8dd3db50
Lucas Amorim <lucas.amorim@ambiensys.com.br>
2026-08-30 04:53:52 -0300

    new buton

--- GitHalls/Views/SyncButton.swift
+++ GitHalls/Views/SyncButton.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct SyncButton: View {
+    @Bindable var viewModel: RepositoryViewModel
+
+    var body: some View {
+        Group {
+            if viewModel.isFetching || viewModel.isPulling || viewModel.isPushing {
+                ProgressView()
+                    .controlSize(.small)
+            } else if !viewModel.hasUpstream {
+                Button {
+                    Task { await viewModel.push() }
+                } label: {
+                    Label("Publish Branch", systemImage: "arrow.up.circle")
+                }
+            } else if viewModel.syncAhead > 0 {
+                Button {
+                    Task { await viewModel.push() }
+                } label: {
+                    Label("Push (\(viewModel.syncAhead))", systemImage: "arrow.up.circle")
+                }
+            } else if viewModel.syncBehind > 0 {
+                Button {
+                    Task { await viewModel.pull() }
+                } label: {
+                    Label("Pull (\(viewModel.syncBehind))", systemImage: "arrow.down.circle")
+                }
+            } else {
+                Label("Up to date", systemImage: "checkmark.circle")
+                    .foregroundStyle(.secondary)
+            }
+        }
+    }
+}
